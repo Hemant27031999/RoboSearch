7bd64a96a04fe6f431ba0974ab52d5d92feb5a49
HemantYadav <hy27.1999@gmail.com>
2019-06-27 00:32:36 +0530

    Updates

---
+++ precache-manifest.bb30196dfca6bfae42947eff12671c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f687e561de4d0e2504d9",
+    "url": "/RoboSearch/static/js/runtime~main.f687e561.js"
+  },
+  {
+    "revision": "8f34c8bdd542ca67eae2",
+    "url": "/RoboSearch/static/js/main.8f34c8bd.chunk.js"
+  },
+  {
+    "revision": "c490bdd62b7985e04649",
+    "url": "/RoboSearch/static/js/1.c490bdd6.chunk.js"
+  },
+  {
+    "revision": "8f34c8bdd542ca67eae2",
+    "url": "/RoboSearch/static/css/main.7d6ec5c8.chunk.css"
+  },
+  {
+    "revision": "c490bdd62b7985e04649",
+    "url": "/RoboSearch/static/css/1.0dbe397a.chunk.css"
+  },
+  {
+    "revision": "1a3f8da3b7998c2fe311f9c6545b9ab2",
+    "url": "/RoboSearch/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.ae6c4e6131bc360a6483c4f88244b148.js"
+  "/RoboSearch/precache-manifest.bb30196dfca6bfae42947eff12671c90.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/RoboSearch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
